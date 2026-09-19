926eaf502815320ac1066741cd3038bc3c78d743
Divyanshu Verma <dv.verma09@gmail.com>
2026-09-19 18:06:40 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,21 @@
+const name = "Divyanshu";
+const age = 20;
+
+console.log(`My name is ${name} and I am ${age} years old.`);
+
+const gameName = new String("Divyanshu");
+console.log(gameName[2]);
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('h'));
+
+const newstring = gameName.substring(0,4);
+console.log(newstring);
+
+const anotherstring = gameName.slice(-8,4);
+console.log(anotherstring);
+
+const url = "abc#gmail.com"
+console.log(url.replace('#','@'));
+
+
+
