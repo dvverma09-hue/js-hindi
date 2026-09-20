3f828e53af1e9238183f9de49421bfcd416ba752
Divyanshu Verma <dv.verma09@gmail.com>
2026-09-20 14:32:01 +0000

    no. and maths cmp.

---
+++ 01_basics/maths.js
@@ -0,0 +1,42 @@
+
+//++++++++++++++++++++++++ No. ++++++++++++++++++++++++++++
+const score = 200
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 124.76;
+//console.log(otherNumber.toPrecision(4));
+
+const hund = 1000000;
+//console.log(hund.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(3.9));
+console.log(Math.min(3,2,4,6,5,8));
+console.log(Math.max(5,2,9,10));
+
+console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
+
+
+
+
+
+
+
+
+
+
+
