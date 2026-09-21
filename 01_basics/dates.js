9b11a1050e9b411ba6998ff50e62921b9f0ca3b2
Divyanshu Verma <dv.verma09@gmail.com>
2026-09-21 13:01:08 +0000

    Dates and time learn

---
+++ 01_basics/dates.js
@@ -0,0 +1,18 @@
+let mydate = new Date();
+console.log(mydate.toDateString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toString());
+
+console.log(typeof(mydate));
+
+let createdate = new Date(2022,0,22);
+console.log(createdate.toDateString());
+
+let myTime = Date.now();
+console.log(myTime);
+console.log(createdate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
+
